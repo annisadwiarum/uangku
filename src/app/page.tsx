f1f703ebb69a42eaf8fbd6b182c6982f574a11c3
annisadwiarum <annisadwiarum710@gmail.com>
2025-05-30 16:58:24 +0700

    test auth-client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { supabaseAdmin } from "@/lib/supabase";
+"use-client";
 
 export default function Home() {
-  const setView = async () => {
-    const { data, error } = await supabaseAdmin
-      .from("views")
-      .insert({ name: "anonym 3" });
-    if (data) console.log(data);
-    if (error) console.log(error);
-  };
-
-  setView();
   return <div>Logged in</div>;
 }
