e8feec3a5ce6ee2a9ee2e1a518de9a151010777a
annisadwiarum <annisadwiarum710@gmail.com>
2025-05-31 17:01:46 +0700

    auth client

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,71 @@
-"use-client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { supabase } from "@/lib/supabase";
+import React, { useState } from "react";
 
-function Login() {
-  return (
-    <div>Login Page</div>
-  )
+interface dataUser {
+  email: string;
+  password: string;
 }
 
-export default Login
+function Login() {
+  const [data, setData] = useState<dataUser>({
+    email: "",
+    password: "",
+  });
+
+  const login = async () => {
+    try {
+      const { data: dataUser, error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (data) console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-200">
+      <div className="flex flex-col gap-5 bg-white w-lg py-10 px-14 rounded-xl shadow-lg border">
+        <h1 className=" text-center font-semibold text-2xl mb-5">Login Page</h1>
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={data?.email}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={data?.password}
+          onChange={handleChange}
+        />
+        <Button
+          variant="secondary"
+          className="bg-gray-200 shadow-sm"
+          onClick={login}
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-"use-client";
-
 export default function Home() {
-  return <div>Logged in</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-10 px-20">
+      <h1 className="font-bold">You Are Logged In</h1>
+    </div>
+  );
 }
