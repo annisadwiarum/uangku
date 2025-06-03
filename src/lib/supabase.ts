ceb5f4220fa960a96daa30f7e927b09ee8896e5e
annisadwiarum <annisadwiarum710@gmail.com>
2025-06-03 17:00:35 +0700

    commit apaan dah

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,10 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export const supabase = createClientComponentClient();
+import { createClient } from "@supabase/supabase-js";
+
+// export const supabase = createClientComponentClient();
+
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
